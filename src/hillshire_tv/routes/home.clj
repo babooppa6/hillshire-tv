0bc5ed60cbdab85cc9ad72a47139c125aba3b38f
Hillshire <jessewchs@gmail.com>
2015-05-26 07:34:53 -0400

    Add 404 route and case insensitive test case

--- src/hillshire_tv/routes/home.clj
+++ src/hillshire_tv/routes/home.clj
@@ -1,6 +1,7 @@
 (ns hillshire-tv.routes.home
   (:require [hillshire-tv.layout :as layout]
             [compojure.core :refer [defroutes GET]]
+            [compojure.route :as route]
             [ring.util.http-response :refer [ok]]
             [clojure.java.io :as io]
             [hillshire-tv.db.core :as db]))
@@ -12,18 +13,22 @@
 
 (defn about-page [] (layout/render "about.html"))
 
+(defn error-page [] (layout/render "error.html"))
+
 (defn custom-page [page]
   ;Render a page's video or image based on its content type
   (let [model (into {} (db/get-page db/db-spec page))]
   (try (layout/render (str (get model :type) ".html") model)
-  (catch Exception e (layout/render "error.html")))))
+  (catch Exception e (route/not-found (error-page))))))
 
 (defn random-page []
   (layout/render "random.html" (into {} (db/get-random db/db-spec))))
+
 
 (defroutes home-routes
   (GET "/" [] (list-pages))
   (GET "/about" [] (about-page))
   (GET "/pages" [] (list-pages))
   (GET "/random" [] (random-page))
-  (GET "/:page" [page] (custom-page (str page))))
+  (GET "/:page" [page] (custom-page (str page)))
+  (route/not-found (error-page)))
--- test/hillshire_tv/test/handler.clj
+++ test/hillshire_tv/test/handler.clj
@@ -12,6 +12,13 @@
     (let [response (app (request :get "/YEE"))]
       (is (= 200 (:status response)))))
 
+  (testing "custom route case insensitive"
+        (let [response (app (request :get "/momo"))
+              response2 (app (request :get "/MoMo"))]
+          (is (= 200 (:status response)))
+          (is (= 200 (:status response2)))
+          (is (= (get response :body) (get response2 :body)))))
+
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= 404 (:status response))))))
