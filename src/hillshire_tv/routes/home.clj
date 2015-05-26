e04ab254e5f3818fb64f532b635a4e97f68ab3d0
Hillshire <jessewchs@gmail.com>
2015-05-26 06:35:21 -0400

    tidy up routes codebase

--- src/hillshire_tv/routes/home.clj
+++ src/hillshire_tv/routes/home.clj
@@ -6,20 +6,18 @@
             [hillshire-tv.db.core :as db]
             [clojure.pprint :refer [pprint]]))
 
-(defn home-page []
-  (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+(defn list-pages []
+  ;Show links to all hillshire.tv pages in a table
+  (let [model {:pages (db/list-pages db/db-spec)}]
+  (layout/render "pagelist.html" model)))
 
 (defn about-page [] (layout/render "about.html"))
 
 (defn custom-page [page]
+  ;Render a page's video or image based on its content type
   (let [model (into {} (db/get-page db/db-spec page))]
   (try (layout/render (str (get model :type) ".html") model)
   (catch Exception e (layout/render "error.html")))))
-
-(defn list-pages []
-  (let [model {:pages (db/list-pages db/db-spec)}]
-  (layout/render "pagelist.html" model)))
 
 (defn random-page []
   (layout/render "random.html" (into {} (db/get-random db/db-spec))))
